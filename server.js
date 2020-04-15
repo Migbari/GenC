57ccc1069cb09a7651ac4b38cdd03ae7e0acd45f
Miguel <miguelbatista.web@gmail.com>
2020-04-15 12:16:05 -0300

    novo servidor

---
+++ server.js
@@ -0,0 +1,7 @@
+﻿var http = require('http'); // Requer um http
+
+// Cria o servidor 
+http.createServer(function(req, res){  
+    res.end("Minha API");
+    console.log("To vivo");
+}).listen(3000);
